6758da71398193660fbcaa03e51577d87476ab8b
Deniz Basocak <denizzr@outlook.com>
2022-05-25 01:17:56 +0300

    Player can move and jump

---
+++ Assets/Scripts/Movement/Player/PlayerMovement.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float _movementSpeed = 2f;
+    [SerializeField] float _jumpForce = 2f;
+
+    PlayerInputAction playerAction;
+    Rigidbody rb;
+
+
+    private void Awake()
+    {
+        playerAction = new PlayerInputAction();
+        playerAction.Player.Enable();
+        playerAction.Player.Jump.performed += HandleJump;
+    }
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+
+    }
+
+    private void HandleJump(InputAction.CallbackContext context)
+    {
+        
+        if (context.performed)
+        {
+           
+            rb.AddForce(Vector3.up * _jumpForce ,ForceMode.VelocityChange);
+        }
+    }
+
+    private void HandleMovement()
+    {
+        float forwardValue = playerAction.Player.Movement.ReadValue<float>();
+
+        rb.AddForce(Vector3.forward * forwardValue * _movementSpeed * Time.fixedDeltaTime, ForceMode.Impulse);
+
+    }
+}
