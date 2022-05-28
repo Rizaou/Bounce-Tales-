145a43d64c1dc704a9db0773da7179b7a7b90ed0
Deniz Basocak <denizzr@outlook.com>
2022-05-29 00:14:53 +0300

    Add Jumping Trigger
    Player jumps only once.
    Player doesn't move while jumping.

--- Assets/Scripts/Movement/Player/PlayerMovement.cs
+++ Assets/Scripts/Movement/Player/PlayerMovement.cs
@@ -10,6 +10,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float _jumpForce = 2f;
     [SerializeField] float maxSpeed = 10f;
 
+
+    [SerializeField] bool isJumping = false;
     PlayerInputAction playerAction;
     Rigidbody rb;
 
@@ -36,19 +38,36 @@ public class PlayerMovement : MonoBehaviour
     private void HandleJump(InputAction.CallbackContext context)
     {
 
+        if (isJumping) { return; }
         if (context.performed)
         {
-
             rb.AddForce(Vector3.up * _jumpForce, ForceMode.VelocityChange);
+            isJumping = true;
         }
     }
 
+
+
     private void HandleMovement()
     {
+        if (isJumping) { return; }
+
         if (Mathf.Abs(rb.velocity.z) > _movementSpeed) { return; }
         float forwardValue = playerAction.Player.Movement.ReadValue<float>();
 
         rb.AddForce(Vector3.forward * forwardValue * _movementSpeed * Time.fixedDeltaTime, ForceMode.VelocityChange);
 
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        /*
+            * Herhangi bir objeye temas ediyorsa isJumping değişkenini false yap.
+
+            * Eğer zaten false ise bir daha false değerini atma.
+        */
+
+        if (!isJumping) { return; }
+        isJumping = false;
     }
 }
