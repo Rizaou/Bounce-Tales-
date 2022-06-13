24346576568ddd96d0db682eb85eeff254f0fe90
Deniz Basocak <denizzr@outlook.com>
2022-06-13 12:49:18 +0300

    Added listeners for the menu buttons
    The button start and exit listeners added.

---
+++ Assets/Scripts/UI/Main Menu/MainMenuUIManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenuUIManager : MonoBehaviour
+{
+    public static MainMenuUIManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void StartButton()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
